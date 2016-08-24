707d5522091cbd7fb1186873e62bf8060f00562e
Bestin John <bjohn@mcfadyen.com>
2016-08-24 08:36:01 +0530

    Restructured the Script delete is working but not updating the UI

--- js/script.js
+++ js/script.js
@@ -3,36 +3,56 @@ $('#goalTable').on('click','.test', function() {
   $(this).parent().parent().toggleClass("highlight")
 });
 
+
 $( document ).ready(function() {
-    function ViewModel(){
+
+    function GoalModel(){
         var self=this;
         self.data = ko.observableArray([
-          { "name": "Javascript","type": "Type 1", "deadline": "2016-08-12" },
-          { "name": "Bootstrap","type": "Type 4","deadline": "2016-08-01"},
-          { "name": "Ajax","type": "Type 3","deadline": "2016-08-04"}
+          { "isSelected":false,"name": "Javascript","type": "Type 1", "deadline": "2016-08-12" },
+          { "isSelected":false,"name": "Bootstrap","type": "Type 4","deadline": "2016-08-01"},
+          { "isSelected":false,"name": "Ajax","type": "Type 3","deadline": "2016-08-04"}
         ]);
 
         self.name = ko.observable();
         self.type = ko.observable();
         self.deadline = ko.observable();
+        self.isSelected = ko.observable(false);
 
         self.selectedItems = ko.observableArray();
 
         self.addGoal=function(){
-          self.data.push({ "name": self.name(),"type": self.type(),"deadline": self.deadline()});
-          self.name("");
-          self.type("");
-          self.deadline("");
-
+          console.log(self.deadline());
+          if(self.name()==""){
+            alert("Enter name for goal");
+          }else if(self.type()=="Choose Type"){
+            alert("Select Type for goal");
+          }else if(self.deadline()==undefined){
+            alert("Choose deadline for goal");
+          }else{
+            self.data.push({ "isSelected":false,"name": self.name(),"type": self.type(),"deadline": self.deadline()});
+            self.name("");
+            self.type("");
+            self.deadline("");
+          }
         }
+
         self.deleteGoal=function(){
-          //self.data.removeAll(self.selectedItems());
-          //self.selectedItems.removeAll();
-          console.log(self.selectedItems());
+
+          for(var i=0;i<self.data().length;i++){
+          if(self.data()[i].isSelected==true){
+            self.data().splice(i,1)
+            i--;
+          }
+
+          }
+
+          // console.log(self.data().splice(0,1));
+          console.log(self.data());
         }
     };
 
-    ko.applyBindings(new ViewModel());
+    ko.applyBindings(new GoalModel());
 
 
 });
